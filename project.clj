bc8123f7abd1e7d3010bd6ba80052eaaff2fe4d3
Gaivile <gaivile.info3@gmail.com>
2016-08-04 23:19:12 +0100

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject show-me-the-money "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
